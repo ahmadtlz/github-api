3c4743253cd3d920e415f7d9643077b07f9eede7
ahmad <ahmad.talezari@profile.ir>
2022-07-19 11:18:24 +0430

    refactor: add url and token in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXTJS_APP_GITHUB_URL: 'https://api.github.com',
+    NEXTJS_APP_GITHUB_TOKEN: 'put-token-here',
+  },
 }
 
 module.exports = nextConfig
