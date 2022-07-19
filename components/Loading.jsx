8ad40465e1e88ef668655addede8a5fa13a66f84
ahmad <ahmad.talezari@profile.ir>
2022-07-19 17:34:14 +0430

    feature: add loading

---
+++ components/Loading.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Box, Spinner} from '@chakra-ui/react'
+
+export default function Loading() {
+  return (
+    <Box
+      py={6}
+      h={'sm'}
+      display={'flex'}
+      flexDir={'column'}
+      justifyContent={'center'}
+      alignItems={'center'}
+    >
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </Box>
+  )
+}
