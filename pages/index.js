1094dc43bfdde8770e77be725853b955f3c979ba
ahmad <ahmad.talezari@profile.ir>
2022-07-19 17:39:59 +0430

    refactor:  add new componnet and remove unused code

--- pages/index.js
+++ pages/index.js
@@ -2,78 +2,45 @@ import {useState, useContext} from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import {getUser} from '../context/GithubAPI'
 import GithubContext from '../context/GithubContext'
+import {
+  Container,
+  FormControl,
+  Input,
+  Button,
+  Box,
+  Spinner,
+  useColorModeValue,
+} from '@chakra-ui/react'
+
+import {getUser} from '../context/GithubAPI'
+
+import UserCard from '../components/UserCard'
+import User from '../components/User'
+import Loading from '../components/Loading'
+import SearchInput from '../components/SearchInput'
+import Layout from '../components/Layout'
 
 export default function Home() {
-  const [search, setSearch] = useState()
-
-  const [text, setText] = useState('')
-
   const {user, dispatch, loading, error} = useContext(GithubContext)
 
-  const handleChange = e => setText(e.target.value)
-
-  const handleSubmit = async e => {
-    e.preventDefault()
-
-    if (text.trim() === '') {
-      dispatch({
-        type: 'GET_GITHUB_FAILURE',
-        payload: 'Please enter something',
-      })
-    } else {
-      dispatch({type: 'SET_LOADING'})
-      const userData = await getUser(text)
-      if (userData.user.message === 'Not Found') {
-        dispatch({
-          type: 'GET_GITHUB_FAILURE',
-          payload: 'Try again, user not found',
-        })
-        return null
-      } else if (userData.user.message === 'Bad credentials') {
-        dispatch({
-          type: 'GET_GITHUB_FAILURE',
-          payload: 'The token needs to be set in configuration file',
-        })
-        return null
-      } else {
-        dispatch({type: 'GET_USER', payload: userData})
-      }
-
-      setText('')
-    }
-  }
-  if (loading) {
-    return <div>loading</div>
-  }
-
-  console.log('error', error)
   return (
-    <div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className="form-control">
-            <div className="relative">
-              <input
-                type="text"
-                className="w-full pr-40 bg-gray-200 input input-lg text-black"
-                placeholder="Search"
-                value={text}
-                onChange={handleChange}
-              />
-              <button
-                type="submit"
-                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
-              >
-                Go
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-      {text && text}
-      <Link href="/profile/defunkt"> get data</Link>
-    </div>
+    <Layout centerContent>
+      <Box my={10}>
+        <Image src="/github.svg" width={124} height={124} alt="logo" />
+      </Box>
+      <SearchInput dispatch={dispatch} loading={loading} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {Boolean(error) ? (
+            <UserCard error={error} message={error} img="/error.svg" />
+          ) : (
+            <User user={user} />
+          )}
+        </>
+      )}
+    </Layout>
   )
 }
