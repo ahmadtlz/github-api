16d1491f5a387248e416a5dac3d1c811405a036d
ahmad <ahmad.talezari@profile.ir>
2022-07-20 17:06:23 +0430

    refactor: add and change components to [githubuser].js

--- pages/profile/[githubuser].jsx
+++ pages/profile/[githubuser].jsx
@@ -1,39 +1,42 @@
 import {useContext, useEffect} from 'react'
-import {useRouter} from 'next/router'
 import {useGetReposAndData} from '../../hooks/useGetReposAndUser'
 import UserCard from '../../components/UserCard'
 import Layout from '../../components/Layout'
+import User from '../../components/User'
+import Loading from '../../components/Loading'
+import Repos from '../../components/Repos'
 
-const GithubUser = props => {
+const GithubUser = () => {
   const {user, loading, repos, dispatch, error} = useGetReposAndData()
 
-  if (loading) {
-    return <div>loading</div>
-  }
-  if (error) {
-    return <div>{error}</div>
-  }
-  const {
-    name,
-    login,
-    avatar_url,
-    location,
-    bio,
-    blog,
-    html_url,
-    followers,
-    following,
-  } = user
-
   return (
-    <Layout>
-      <UserCard
-        followers={followers}
-        following={following}
-        img={avatar_url}
-        name={login}
-        profile={true}
-      />
+    <Layout
+      display={'flex'}
+      flexDir={['column', 'row']}
+      justifyContent={'space-between'}
+      px={0}
+      width={'100%'}
+      maxW={'100%'}
+    >
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {Boolean(error) ? (
+            <UserCard
+              error={error}
+              message={error}
+              img="/error.svg"
+              profile={true}
+            />
+          ) : (
+            <>
+              <User user={Boolean(user.message) ? [] : user} profile={true} />
+              <Repos repos={Boolean(repos.message) ? [] : repos} />
+            </>
+          )}
+        </>
+      )}
     </Layout>
   )
 }
